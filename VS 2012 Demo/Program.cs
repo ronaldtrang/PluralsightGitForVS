48729c272d13a14e9184ae89b6d0e86fd38d3230
Ronald Trang <ronaldtrang@outlook.com>
2016-06-21 11:00:08 -0700

    Initial Commit

---
+++ VS 2012 Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_2012_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Local changes
+
+            //some more changes
+
+            //more changes
+
+            //last change before push
+
+            //Feature 1.1 change
+
+            //Additional 1.1 change  
+        }
+    }
+}
